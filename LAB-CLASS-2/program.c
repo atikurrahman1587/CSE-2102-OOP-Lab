0867e39c53f8156a45af8014724ad6875f84f382
Md Atikur Rahman <73924182+atikurrahman1587@users.noreply.github.com>
2025-02-02 21:25:39 +0600

    lab task 2

---
+++ LAB-CLASS-2/program.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    printf("%d", sum(10, 20));
+}
+
+int sum(int a, int b){
+    return a+b;
+}
---
+++ LAB-CLASS-2/program.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+void OddEven(){
+    int x;
+    cin>>x;
+    if(x%2 == 0){
+        cout<<"EVEN"<<endl;
+    }else {
+        cout<<"ODD"<<endl;
+    }
+}
+
+int main()
+{
+    OddEven();
+
+    return 0;
+}
+
